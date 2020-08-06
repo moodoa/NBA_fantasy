d398b8c6dd3023486c48cb017c80740839a1a3e9
weed <aa469413927@gmail.com>
2020-08-06 18:59:17 +0800

    <refactor> refactoring

--- predict.py
+++ predict.py
@@ -119,7 +119,42 @@ class NBAPredict:
         players_table = players_table[players_table['team'] != '']
         return players_table
 
+    # selenium issue to solve
+    def _set_back_to_back(self, team_tmr):       
+        yesterday = datetime.datetime.now() - datetime.timedelta(days= 1)
+        m, d, y = yesterday.month, yesterday.day, yesterday.year
+        try:
+            driver = self.webdriver
+            driver.get(f'https://stats.nba.com/players/traditional/?DateFrom={m}%2F{d}%2F{y}')
+            time.sleep(5)
+            table = driver.find_element_by_class_name('nba-stat-table__overflow')
+            stat_yesterday = table.text
+            time.sleep(5)
+            driver.close()
+            split_stat = stat_yesterday.split('\n')
+            b2b_teams = []
+            for idx in range(1,len(split_stat)):
+                try:
+                    team = split_stat[3*idx].split(' ')[0]
+                    if team not in b2b_teams:
+                        b2b_teams.append(team)
+                except IndexError:
+                    pass
+        except:
+            driver.close()
+            b2b_teams = []
+            pass
+        team_tmr['b2b'] = team_tmr['team'].apply(lambda x : True if x in b2b_teams else False)
+        no_b2b_teams = team_tmr[team_tmr['b2b'] == False]
+        return no_b2b_teams
 
+    def _append_cost(self, team_tmr, players_status):
+        cost_dict = {}
+        for idx in range(len(players_status)):
+            cost_dict[players_status[idx]['firstName']+' '+players_status[idx]['lastName']] = int(players_status[idx]['rating'])
+        team_tmr['cost'] = team_tmr['Player'].apply(lambda player_name:self._is_same_player(cost_dict, player_name))
+        team_tmr = team_tmr[team_tmr['cost'].notnull()]
+        return team_tmr
 
 
 # run
@@ -128,226 +163,185 @@ class NBAPredict:
         player_table = self._concat_daily_stat()
         player_table_with_position_team = self._append_position_team(player_table, players_status)
         team_tmr = self._team_play_tomorrow(player_table_with_position_team)
+        # no_b2b_teams = self._set_back_to_back(team_tmr)
+        team_tmr = self._append_cost(team_tmr, players_status)
+        return team_tmr
 
-# TODO
-down there
-
-
-# b2b check
-m = datetime.datetime.now().month
-d = datetime.datetime.now().day-1
-y = datetime.datetime.now().year
-try:
-    browser = webdriver.Chrome()
-    browser.get('https://stats.nba.com/players/traditional/?DateFrom='+str(m)+'%2F'+str(d)+'%2F'+str(y)+'&sort=PTS&dir=-1')
-    time.sleep(2)
-    table = browser.find_element_by_class_name('nba-stat-table__overflow')
-    daily_str = table.text
-    browser.close()
-    split_data = daily_str.split('\n')
-
-    b2b_teams = []
-    for i in range(1,len(split_data)):
-        try:
-            datalist = split_data[3*i]
-            datalist = datalist.split(' ')[0]
-            if datalist not in b2b_teams:
-                b2b_teams.append(datalist)
-        except IndexError:
-            pass
-except:
-    browser.close()
-    b2b_teams = []
-    pass
-
-top_table['b2b'] = top_table['Team'].apply(lambda x : True if x in b2b_teams else False)
-
-top_table = top_table[top_table['b2b'] == False]
+if __name__ == '__main__':
+    predictor = NBAPredict('aa469413927@yahoo.com.tw','a358302916')
+    print(predictor.predict())
 
 
 
-# add cost
-
-cost_dict = {}
-
-for i in range(len(players_status)):
-    cost_dict[players_status[i]['firstName']+' '+players_status[i]['lastName']] = int(players_status[i]['rating'])
 
 
 
-def checkcost(name):
-    for n in cost_dict.keys():
-        if fuzz.ratio(name,n) >= 80:
-            return cost_dict[n]
-
-top_table['cost'] = top_table['Player'].apply(lambda x: checkcost(x))
-
-tmr_set = top_table[top_table['cost'].notnull()]
-
-
-# injury list
-
-# get the injury list from web
-url = requests.get('https://www.cbssports.com/nba/injuries/')
-
-content = url.content
-
-soup = BeautifulSoup(content,'html.parser')
-
-table = soup.find(name= 'div' ,attrs= {'class':'Page-colMain'})
-
-html_str = str(table)
-
-data2 = pd.read_html(html_str)[0]
-
-# concat all teams list
-for i in range(1,31):
-    try:
-        for_concat = pd.read_html(html_str)[i]
-        data2 = pd.concat([data2,for_concat])
-    except IndexError:
-        break
-# fix the name and injury stat
-data2['Injury_man'] = data2['Player'].apply(lambda x:x.split(' ',2)[2])
-# data2 = data2.reset_index()
-data2 = data2.sort_values('Injury Status')
-data2['status'] = data2['Injury Status'].apply(lambda x :x.split(' ',7)[7] if len(x)>18 else x )
-data2['status'] = data2['status'].apply(lambda x :x.split(' ',1)[0] if len(x)>10 else x )
-data2 = data2.loc[:,['Injury_man','status']]
-data2 = data2.set_index('status')
-
-injury_list = data2['Injury_man'].values
-
-def injurycheck(name):
-    for n in injury_list:
-        if fuzz.ratio(name,n)>=80:
-            return True
-    return False
-
-tmr_set['injury'] = tmr_set['Player'].apply(lambda x: injurycheck(x))
-
-tmr_set = tmr_set[tmr_set['injury'] == False]
-
-
-# arena for home and guest
-
-mon = datetime.datetime.now().month
-day = datetime.datetime.now().day
-day = str(day)
-if len(day)<2:
-    day = '0'+day
-web = requests.get('https://stats.nba.com/scores/0'+str(mon)+'/'+day+'/2020')
-content = web.content
-soup = BeautifulSoup(content,'html.parser')
-web_string = str(soup)
-team_messy = re.findall(r'\d+\\/\D{6}',web_string)
-team_tmr = []
-for t in team_messy:
-    team_tmr.append(t.split('/')[1])
-team_home = []
-team_guest = []
-for t in team_tmr:
-    team_guest.append(t[0:3])
-    team_home.append(t[3::])
-
-home = pd.DataFrame()
-
-for t in team_home:
-    for_concat = tmr_set[(tmr_set['Team'] == t)&(tmr_set['arena'] == 'home')]
-    home = pd.concat([home,for_concat])
-
-guest = pd.DataFrame()
-
-for t in team_guest:
-    for_concat = tmr_set[(tmr_set['Team'] == t)&(tmr_set['arena'] == 'guest')]
-    guest = pd.concat([guest,for_concat])
-
-
-tmr_set = pd.concat([home,guest])
-
-
-# make a filter and weight the AVG 
-
-tmr_set = tmr_set[tmr_set['AVG']>(tmr_set['AVG'].mean())*1.25]
-tmr_set = tmr_set[tmr_set['cost']>=75]
-
-tmr_set.reset_index(inplace=True)
-# classfy players by position
-
-guards = tmr_set[(tmr_set['position'] == 'G') | (tmr_set['position'] == 'F-G')]
-forwards = tmr_set[(tmr_set['position'] == 'F') | (tmr_set['position'] == 'F-G')| (tmr_set['position'] == 'C-F')]
-centers = tmr_set[(tmr_set['position'] == 'C') | (tmr_set['position'] == 'C-F')]
 
 
 
-# suggestion
-team = []
+# # injury list
 
-for player1 in range(len(guards)-1):
-    for player2 in range(player1+1,len(guards)):
-        for player3 in range(len(forwards)-1):
-            for player4 in range(player3+1,len(forwards)):
-                for player5 in range(len(centers)):
-                    if (guards['cost'].values[player1]+guards['cost'].values[player2]\
-                        +forwards['cost'].values[player3]+forwards['cost'].values[player4]\
-                        +centers['cost'].values[player5]<=430)\
-                        and(guards['cost'].values[player1]+guards['cost'].values[player2]\
-                            +forwards['cost'].values[player3]+forwards['cost'].values[player4]\
-                            +centers['cost'].values[player5]>=420):
-                        team.append([guards['Player'].values[player1],
-                                    guards['Player'].values[player2],
-                                    forwards['Player'].values[player3],
-                                    forwards['Player'].values[player4],
-                                    centers['Player'].values[player5]])
+# # get the injury list from web
+# url = requests.get('https://www.cbssports.com/nba/injuries/')
+
+# content = url.content
+
+# soup = BeautifulSoup(content,'html.parser')
+
+# table = soup.find(name= 'div' ,attrs= {'class':'Page-colMain'})
+
+# html_str = str(table)
+
+# data2 = pd.read_html(html_str)[0]
+
+# # concat all teams list
+# for i in range(1,31):
+#     try:
+#         for_concat = pd.read_html(html_str)[i]
+#         data2 = pd.concat([data2,for_concat])
+#     except IndexError:
+#         break
+# # fix the name and injury stat
+# data2['Injury_man'] = data2['Player'].apply(lambda x:x.split(' ',2)[2])
+# # data2 = data2.reset_index()
+# data2 = data2.sort_values('Injury Status')
+# data2['status'] = data2['Injury Status'].apply(lambda x :x.split(' ',7)[7] if len(x)>18 else x )
+# data2['status'] = data2['status'].apply(lambda x :x.split(' ',1)[0] if len(x)>10 else x )
+# data2 = data2.loc[:,['Injury_man','status']]
+# data2 = data2.set_index('status')
+
+# injury_list = data2['Injury_man'].values
+
+# def injurycheck(name):
+#     for n in injury_list:
+#         if fuzz.ratio(name,n)>=80:
+#             return True
+#     return False
+
+# tmr_set['injury'] = tmr_set['Player'].apply(lambda x: injurycheck(x))
+
+# tmr_set = tmr_set[tmr_set['injury'] == False]
 
 
-allteam = len(team)
-score_dict={}
+# # arena for home and guest
 
-for i in range(allteam):
-    score = guards[tmr_set['Player'] == team[i][0]]['AVG'].values[0]\
-        +guards[tmr_set['Player'] == team[i][1]]['AVG'].values[0]\
-        +forwards[tmr_set['Player'] == team[i][2]]['AVG'].values[0]\
-        +forwards[tmr_set['Player'] == team[i][3]]['AVG'].values[0]\
-        +centers[tmr_set['Player'] == team[i][4]]['AVG'].values[0]  
-    score_dict[i] = score
+# mon = datetime.datetime.now().month
+# day = datetime.datetime.now().day
+# day = str(day)
+# if len(day)<2:
+#     day = '0'+day
+# web = requests.get('https://stats.nba.com/scores/0'+str(mon)+'/'+day+'/2020')
+# content = web.content
+# soup = BeautifulSoup(content,'html.parser')
+# web_string = str(soup)
+# team_messy = re.findall(r'\d+\\/\D{6}',web_string)
+# team_tmr = []
+# for t in team_messy:
+#     team_tmr.append(t.split('/')[1])
+# team_home = []
+# team_guest = []
+# for t in team_tmr:
+#     team_guest.append(t[0:3])
+#     team_home.append(t[3::])
+
+# home = pd.DataFrame()
+
+# for t in team_home:
+#     for_concat = tmr_set[(tmr_set['Team'] == t)&(tmr_set['arena'] == 'home')]
+#     home = pd.concat([home,for_concat])
+
+# guest = pd.DataFrame()
+
+# for t in team_guest:
+#     for_concat = tmr_set[(tmr_set['Team'] == t)&(tmr_set['arena'] == 'guest')]
+#     guest = pd.concat([guest,for_concat])
 
 
-# get the predict score and index
-rank = sorted(score_dict.items(), key=lambda d: d[1],reverse=True)
-teamcheck = []
-team_number = 0
-medal = 0
-predict = pd.DataFrame()
-for i in range(len(rank)):
-    player_dict={'team':str(medal+1)}
-    teamlist=[]
-    for player in team[rank[i][0]]:
-        if player in teamlist:
-            break
-        else:
-            teamlist.append(player)
-    sort_team = sorted(teamlist)
-    if len(teamlist) == 5 and sort_team not in teamcheck:
-        teamcheck.append(sort_team)
-        medal+=1
-        for j in range(len(teamlist)):
-            player_dict[teamlist[j]]=tmr_set[tmr_set['Player']==teamlist[j]]['Team'].values[0]
-        for_concat = pd.DataFrame(list(player_dict.items()))
-        predict = pd.concat([predict,for_concat])
+# tmr_set = pd.concat([home,guest])
+
+
+# # make a filter and weight the AVG 
+
+# tmr_set = tmr_set[tmr_set['AVG']>(tmr_set['AVG'].mean())*1.25]
+# tmr_set = tmr_set[tmr_set['cost']>=75]
+
+# tmr_set.reset_index(inplace=True)
+# # classfy players by position
+
+# guards = tmr_set[(tmr_set['position'] == 'G') | (tmr_set['position'] == 'F-G')]
+# forwards = tmr_set[(tmr_set['position'] == 'F') | (tmr_set['position'] == 'F-G')| (tmr_set['position'] == 'C-F')]
+# centers = tmr_set[(tmr_set['position'] == 'C') | (tmr_set['position'] == 'C-F')]
+
+
+
+# # suggestion
+# team = []
+
+# for player1 in range(len(guards)-1):
+#     for player2 in range(player1+1,len(guards)):
+#         for player3 in range(len(forwards)-1):
+#             for player4 in range(player3+1,len(forwards)):
+#                 for player5 in range(len(centers)):
+#                     if (guards['cost'].values[player1]+guards['cost'].values[player2]\
+#                         +forwards['cost'].values[player3]+forwards['cost'].values[player4]\
+#                         +centers['cost'].values[player5]<=430)\
+#                         and(guards['cost'].values[player1]+guards['cost'].values[player2]\
+#                             +forwards['cost'].values[player3]+forwards['cost'].values[player4]\
+#                             +centers['cost'].values[player5]>=420):
+#                         team.append([guards['Player'].values[player1],
+#                                     guards['Player'].values[player2],
+#                                     forwards['Player'].values[player3],
+#                                     forwards['Player'].values[player4],
+#                                     centers['Player'].values[player5]])
+
+
+# allteam = len(team)
+# score_dict={}
+
+# for i in range(allteam):
+#     score = guards[tmr_set['Player'] == team[i][0]]['AVG'].values[0]\
+#         +guards[tmr_set['Player'] == team[i][1]]['AVG'].values[0]\
+#         +forwards[tmr_set['Player'] == team[i][2]]['AVG'].values[0]\
+#         +forwards[tmr_set['Player'] == team[i][3]]['AVG'].values[0]\
+#         +centers[tmr_set['Player'] == team[i][4]]['AVG'].values[0]  
+#     score_dict[i] = score
+
+
+# # get the predict score and index
+# rank = sorted(score_dict.items(), key=lambda d: d[1],reverse=True)
+# teamcheck = []
+# team_number = 0
+# medal = 0
+# predict = pd.DataFrame()
+# for i in range(len(rank)):
+#     player_dict={'team':str(medal+1)}
+#     teamlist=[]
+#     for player in team[rank[i][0]]:
+#         if player in teamlist:
+#             break
+#         else:
+#             teamlist.append(player)
+#     sort_team = sorted(teamlist)
+#     if len(teamlist) == 5 and sort_team not in teamcheck:
+#         teamcheck.append(sort_team)
+#         medal+=1
+#         for j in range(len(teamlist)):
+#             player_dict[teamlist[j]]=tmr_set[tmr_set['Player']==teamlist[j]]['Team'].values[0]
+#         for_concat = pd.DataFrame(list(player_dict.items()))
+#         predict = pd.concat([predict,for_concat])
         
-    if medal == 3 :
-        break
-predict.columns=['Player','Team']
+#     if medal == 3 :
+#         break
+# predict.columns=['Player','Team']
 
-predict = predict.set_index('Team')
+# predict = predict.set_index('Team')
 
 
-# send the result to line
-def send_ifttt(data):   
-#     send the report to line
-    url = ('https://maker.ifttt.com/trigger/nba_fantasy/with/key/btssJUCF_1qKOVluaYsMC1' +
-          '?value1='+str(data))
-#     action!!
-    r = requests.get(url) 
-result = send_ifttt(predict)
+# # send the result to line
+# def send_ifttt(data):   
+# #     send the report to line
+#     url = ('https://maker.ifttt.com/trigger/nba_fantasy/with/key/btssJUCF_1qKOVluaYsMC1' +
+#           '?value1='+str(data))
+# #     action!!
+#     r = requests.get(url) 
+# result = send_ifttt(predict)
